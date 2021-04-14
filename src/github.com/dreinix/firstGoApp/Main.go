30c214376267e99449e2473d58bb5a14e5850d85
dreinix <ricardo.aar@outlook.com>
2021-04-14 01:47:36 -0400

    Variaable Convetion to string

--- src/github.com/dreinix/firstGoApp/Main.go
+++ src/github.com/dreinix/firstGoApp/Main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+var date = 16
 
 func main() {
-	i := 42
-	fmt.Println(i)
+	/*var date int
+	date = 16*/
+	var month int = 12
+	year := 99
+	converted := strconv.Itoa(year)
+	fmt.Printf("%v %v %v", date, month, converted)
 }
